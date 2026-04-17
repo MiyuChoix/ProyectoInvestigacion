a65ff23a6ae32f5744d3da7bb0cc44e63e87323c
Juan Pablo Saenz Lopez <maxunlimit10@gmail.com>
2026-04-17 03:29:22 -0600

    Se corrigen errores en la estructura del repositorio main. Se agrega archivo sql para compartir la base de datos utilizada. Entre otros cambios que olvide mencionar anteriormente.

---
+++ asesoria.sql
@@ -0,0 +1,160 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: asesoria
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `asesor_materias`
+--
+
+DROP TABLE IF EXISTS `asesor_materias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asesor_materias` (
+  `idAsesor` int(11) NOT NULL,
+  `idMateria` int(11) NOT NULL,
+  PRIMARY KEY (`idAsesor`,`idMateria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asesor_materias`
+--
+
+LOCK TABLES `asesor_materias` WRITE;
+/*!40000 ALTER TABLE `asesor_materias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asesor_materias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `asesores`
+--
+
+DROP TABLE IF EXISTS `asesores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asesores` (
+  `idAsesor` int(11) NOT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `nControl` varchar(45) DEFAULT NULL,
+  `contrasena` varchar(45) NOT NULL,
+  `carrera` varchar(45) NOT NULL,
+  `semestre` int(11) NOT NULL,
+  `fechaRegistro` date DEFAULT current_timestamp(),
+  `nombre` varchar(60) NOT NULL,
+  `apellidos` varchar(60) NOT NULL,
+  PRIMARY KEY (`idAsesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='ola';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asesores`
+--
+
+LOCK TABLES `asesores` WRITE;
+/*!40000 ALTER TABLE `asesores` DISABLE KEYS */;
+INSERT INTO `asesores` VALUES (1,'L23550737@chihuahua2.tecnm.mx','23550737','holamundo','IINF',6,'2026-04-17','Juan Pablo','Saenz Lopez'),(2,'L23550740@chihuahua2.tecnm.mx','23550740','holamundo','IINF',6,'2026-04-17','Miyuki','Choix Valdez');
+/*!40000 ALTER TABLE `asesores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudiantes`
+--
+
+DROP TABLE IF EXISTS `estudiantes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudiantes` (
+  `idEstudiante` int(11) NOT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `nControl` varchar(45) DEFAULT NULL,
+  `contrasena` varchar(45) NOT NULL,
+  `carrera` varchar(45) NOT NULL,
+  `semestre` int(11) NOT NULL,
+  `fechaRegistro` date DEFAULT current_timestamp(),
+  PRIMARY KEY (`idEstudiante`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudiantes`
+--
+
+LOCK TABLES `estudiantes` WRITE;
+/*!40000 ALTER TABLE `estudiantes` DISABLE KEYS */;
+INSERT INTO `estudiantes` VALUES (1,'L23550737@chihuahua2.tecnm.mx','23550737','5981761028','IINF',6,'2026-04-14'),(2,'L23550738@chihuahua2.tecnm.mx','23550738','15151515a','IINF',6,'2026-04-14'),(3,'L23550740@chihuahua2.tecnm.mx','23550740','3197462742','IINF',6,'2026-04-14');
+/*!40000 ALTER TABLE `estudiantes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historial_sesiones`
+--
+
+DROP TABLE IF EXISTS `historial_sesiones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial_sesiones` (
+  `idSesion` int(11) NOT NULL,
+  `idAsesor` int(11) NOT NULL,
+  `idEstudiante` int(11) NOT NULL,
+  `materia` varchar(45) NOT NULL,
+  `fecha` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`idSesion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial_sesiones`
+--
+
+LOCK TABLES `historial_sesiones` WRITE;
+/*!40000 ALTER TABLE `historial_sesiones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historial_sesiones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materias`
+--
+
+DROP TABLE IF EXISTS `materias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materias` (
+  `idMateria` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idMateria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materias`
+--
+
+LOCK TABLES `materias` WRITE;
+/*!40000 ALTER TABLE `materias` DISABLE KEYS */;
+INSERT INTO `materias` VALUES (1,'Fisica'),(2,'Calculo Diferencial'),(3,'Administracion de la Funcion Informatica'),(4,'Calculo Integral'),(5,'Programacion Orientada a Objetos'),(6,'el papu el papu oyea oyea');
+/*!40000 ALTER TABLE `materias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-17  3:25:33
